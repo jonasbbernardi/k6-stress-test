697842d851ea649fd1f06700519a616de0b9c001
Jonas B. Bernardi <jonas.bernardi@duosystem.com.br>
2025-07-14 14:19:11 -0300

    doc: comments

--- src/test2.ts
+++ src/test2.ts
@@ -15,30 +15,35 @@ export const scenario = {
 export async function test() {
   const context = await browser.newContext();
   const page = await context.newPage();
+  const ss_path = 'results/screenshots';
 
   try {
+    // Access page
     await page.goto('https://test.k6.io/my_messages.php');
 
+    // Screenshot home and fill login form
     await Promise.all([
-      page.screenshot({ path: 'screenshots/home.png' }),
+      page.screenshot({ path: `${ss_path}/home.png` }),
       page.locator('input[name="login"]').type('admin'),
       page.locator('input[name="password"]').type('123'),
     ])
-    await page.screenshot({ path: 'screenshots/login-form-filled.png' });
 
+    // Screenshot login from fielled
+    await page.screenshot({ path: `${ss_path}/login-form-filled.png` });
+
+    // Click on submit and wait navigation (document load)
     await Promise.all([
       page.waitForNavigation(),
       page.locator('input[type="submit"]').click(),
     ]);
     
     // Wait for react loading
-    // await Promise.all([
-    //   page.locator('button').click(),
-    //   page.screenshot({ path: 'screenshots/login-waiting.png' }),
-    //   page.waitForLoadState('networkidle')
-    // ])
-    await page.screenshot({ path: 'screenshots/logged-in.png' });
+    // await page.waitForLoadState('networkidle');
 
+    // Screenshot logged in page
+    await page.screenshot({ path: `${ss_path}/logged-in.png` });
+
+    // Validate welcome header
     await check(page.locator('h2'), {
       header: async (h2) => (await h2.textContent()) == 'Welcome, admin!'
     });
